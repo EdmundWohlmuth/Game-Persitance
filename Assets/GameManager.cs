7e5b070f514d54ceea7f9705795533d9f2ebf065
Edmund Wohlmuth <74564582+EdmundWohlmuth@users.noreply.github.com>
2022-10-16 12:16:53 -0300

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameObject Player;
+    private Movement player;
+    public GameObject UIManager;
+
+    public float health = 100;
+    public float xp = 0;
+    public float score = 0;
+    public float level = 0;
+    public Vector3 playerPos;
+    public float shield = 100;
+
+    private void Awake()
+    {
+       if (instance == null)
+       {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+       }
+       else if (instance != this)
+       {
+            Destroy(gameObject);
+       }
+    }
+
+    void Start()
+    {
+        player = GetComponent<Movement>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SaveGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat"); //creates file path
+
+        PlayerData data = new PlayerData(); // sets values for save file
+        data.health = health;
+        data.xp = xp;
+        data.score = score;
+        data.level = level;
+        data.playerPos = player.position;
+        data.shield = shield;
+
+        bf.Serialize(file, data); // writes to file
+        file.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            health = data.health;
+            xp = data.xp;
+            score = data.score;
+            level = data.level;
+            player.position = data.playerPos;
+            shield = data.shield;
+        }
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float health;
+        public float xp;
+        public float score;
+        public float level;
+        public Vector3 playerPos;
+        public float shield;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Movement : MonoBehaviour
+{
+    public GameManager GM;
+
+    float acceleration = 0.1f; // ??? getting applied every frame?
+    public Vector3 position; // in units
+    Vector3 velocity; // in units/second
+    int levelUpAmmount;
+
+    [Header("text")]
+    public TMP_Text xp;
+    public TMP_Text level;
+    public TMP_Text shield;
+    public TMP_Text health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set position
+        position.x = 0;
+        position.y = 1;
+        position.z = -10;
+
+        // set velocity
+        velocity.x = 0;
+        velocity.y = 0;
+        velocity.z = 0;
+
+        //set UI
+        xp.text = "XP: " + GM.xp.ToString();
+        level.text = "Level: " + GM.level.ToString();
+        shield.text = "Shield: " + GM.shield.ToString();
+        health.text = "Health: " + GM.health.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        ///reading/processing inupt
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector3.right;
+        }
+
+        // apply friction
+        velocity *=  1f - (1.5f * Time.deltaTime);
+
+        //apply velocity
+        position += velocity * acceleration * Time.deltaTime;
+
+        // only for unity --- .draw would already use ^
+        gameObject.transform.position = position;
+
+        UIControls();
+    }
+
+    void UIControls()
+    {
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        {
+            //save
+            GM.SaveGame();
+        }
+        else if (Input.GetKey(KeyCode.Keypad0))
+        {
+            //load
+            GM.Load();
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            //decrement health
+            GM.shield -= 5;
+            if (GM.shield <= 0)
+            {
+                GM.health += GM.shield;
+                GM.shield = 0;
+                if (GM.health <= 0)
+                {
+                    GM.health = 0;
+                    GM.shield = 0;
+                }
+            }
+            shield.text = "Shield: " + GM.shield.ToString();
+            health.text = "Health: " + GM.health.ToString();
+        }
+        else if (Input.GetKeyDown(KeyCode.Equals))
+        {
+            //incriment xp
+            GM.xp += 5;
+            xp.text = "XP: " + GM.xp.ToString();
+            if (GM.xp > levelUpAmmount)
+            {
+                GM.level++;
+                levelUpAmmount += 15;
+                level.text = "Level: " + GM.level.ToString();
+            }
+        }
+
+    }
+}
